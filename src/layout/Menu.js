3f9cededcea34f94d9a96b01b41ab246867ae1f4
suhye0n <contact.suhyeon@gmail.com>
2023-10-26 16:10:24 +0900

    메뉴 변경

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -6,8 +6,8 @@ const Menu = () => {
         <div className="menu">
             <Link to="/"><FiHome className="menu-icon" /><p>홈</p></Link>
             <Link to="/custom-policy"><FiFileText className="menu-icon" /><p>맞춤 정책</p></Link>
-            <Link to="/local-news"><FiActivity className="menu-icon" /><p>동네 소식</p></Link>
-            <Link to="/policy-news"><FiBook className="menu-icon" /><p>후기 소식</p></Link>
+            <Link to="/local-news"><FiActivity className="menu-icon" /><p>후기 소식</p></Link>
+            <Link to="/policy-news"><FiBook className="menu-icon" /><p>정책 소식</p></Link>
             <Link to="/mypage"><FiUser className="menu-icon" /><p>마이페이지</p></Link>
         </div>
     );
