ee37a4478c6be9e670ae0cfc9aa4904256d24cf6
leechanyoung2 <147119779+leechanyoung2@users.noreply.github.com>
2023-10-31 17:27:23 +0900

    feat: 설정 기능 추가

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,62 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { FaAngleRight } from 'react-icons/fa';
+import Modal from '../layout/Modal';
 
 const Setting = () => {
-    return (
-        <>
-            <div classname = "container">
-                <h2>설정</h2>
-            </div>
-            <br />
-            <div className="lists">
-                <h3>설정</h3>
-                <table>
-                <div>
-                    <a href="">알림 설정</a>
-                </div>
-                <div>
-                    <a href="">계정 연동</a>
-                </div>
-                </table>
-                
-            </div>
-            <br />
-            <div className="lists">
-                <h3>이용 안내</h3>
-                <table>
-                    <div>
-                        <a href="">공지사항</a>
-                    </div>
-                    <div>
-                        <a href="">자주 묻는 질문</a>
-                    </div>
-                    <div>
-                        <a href="">고객 문의</a>
-                    </div>
-                </table>
-            </div>
-            <br />
-            <div className="lists">
-                <h3>이용 정보</h3>
-                <table>
-                    <div>
-                        <a href="">이용 약관</a>
-                    </div>
-                    <div>
-                        <a href="">개인정보 처리방침</a>
-                    </div>
-                    <div>
-                        <a href="">마케팅 이용동의</a>
-                    </div>
-                    <div>
-                        <a href="">로그아웃</a>
-                    </div>
-                </table>
-            </div>
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
 
+  const openModal = (content) => {
+    setModalContent(content);
+    setModalOpen(true);
+  };
 
-        </>
-    )
-}
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h3>설정</h3>
+        <div className="section">
+          <div className="title">이용 안내</div>
+          <p className="list" onClick={() => openModal('자주 묻는 질문')}>자주 묻는 질문 <FaAngleRight /></p>
+        </div>
+
+        <div className="section">
+          <div className="title">이용 정보</div>
+          <p className="list" onClick={() => openModal('이용 약관')}>이용 약관 <FaAngleRight /></p>
+          <p className="list" onClick={() => openModal('개인정보 처리방침')}>개인정보 처리방침 <FaAngleRight /></p>
+          <p className="list" onClick={() => openModal('로그아웃')}>로그아웃 <FaAngleRight /></p>
+        </div>
+
+        <div className="withdraw" onClick={() => openModal('회원 탈퇴')}>회원 탈퇴</div>
+      </div>
+
+      <Modal isOpen={modalOpen} onClose={closeModal}>
+        {modalContent}
+      </Modal>
+    </>
+  );
+};
 
 export default Setting;
