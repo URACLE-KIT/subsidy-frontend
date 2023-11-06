7e7376eec78c3b360743fdcb38e9f5bfe87adc0f
suhye0n <contact.suhyeon@gmail.com>
2023-11-06 10:36:48 +0900

    chore: 프록시 설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/',
+    '/auth',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'http://localhost:8080/auth',
       changeOrigin: true,
     })
   );
