c096df213f2fa0f24f530e4789ff009398af64d7
suhye0n <contact.suhyeon@gmail.com>
2023-11-06 11:00:37 +0900

    style: 메뉴 글 수정

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -12,9 +12,9 @@ const Menu = () => {
     return (
         <div className="menu">
             <Link to="/" className={isActive('/') ? 'active' : ''}><FiHome className="menu-icon" /><p>홈</p></Link>
-            <Link to="/custom" className={isActive('/custom') ? 'active' : ''}><FiFileText className="menu-icon" /><p>맞춤 정책</p></Link>
-            <Link to="/review" className={isActive('/review') ? 'active' : ''}><FiActivity className="menu-icon" /><p>후기 소식</p></Link>
-            <Link to="/latest" className={isActive('/latest') ? 'active' : ''}><FiBook className="menu-icon" /><p>정책 소식</p></Link>
+            <Link to="/custom" className={isActive('/custom') ? 'active' : ''}><FiFileText className="menu-icon" /><p>맞춤알리미</p></Link>
+            <Link to="/review" className={isActive('/review') ? 'active' : ''}><FiActivity className="menu-icon" /><p>후기알리미</p></Link>
+            <Link to="/latest" className={isActive('/latest') ? 'active' : ''}><FiBook className="menu-icon" /><p>홍보알리미</p></Link>
             <Link to="/mypage" className={isActive('/mypage') ? 'active' : ''}><FiUser className="menu-icon" /><p>마이페이지</p></Link>
         </div>
     );
