a3e0be0efbea7f45ac3a83ee4c3df4ad1dcbd7c2
suhye0n <contact.suhyeon@gmail.com>
2023-11-06 11:27:03 +0900

    chore: 프록시 설정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
         const password = document.getElementById("password").value;
 
         try {
-            const response = await axios.post('/auth/signin', {
+            const response = await axios.post('/api/auth/signin', {
                 email: email,
                 password: password
             });
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/auth',
+    '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080/auth',
+      target: 'http://localhost:8080/',
       changeOrigin: true,
     })
   );
