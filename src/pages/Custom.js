af724bdf6bc4c2fb4fdaa5ee4aa57b4de2271e99
suhye0n <contact.suhyeon@gmail.com>
2023-11-07 09:49:55 +0900

    fix: 카테고리 필터 오류 해결

--- src/pages/Custom.js
+++ src/pages/Custom.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { FaBookmark, FaRegBookmark } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Custom = () => {
+  const location = useLocation();
+  const search = new URLSearchParams(location.search).get("search");
+  const option = new URLSearchParams(location.search).get("option");
   const [filter, setFilter] = useState("전체");
   const [policies, setPolicies] = useState([]);
   const [name, setName] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
   const [filteredPolicies, setFilteredPolicies] = useState([]);
-  const filterOptions = ["전체"];
 
   useEffect(() => {
     const storedName = M.data.storage('name');
@@ -20,20 +22,33 @@ const Custom = () => {
   }, []);
 
   useEffect(() => {
-    axios.get("/v1/subsidies/all")
+    let requestURL = "/v1/subsidies/all";
+
+    if (search && option) {
+      requestURL = `/v1/subsidies/search/${option}?${option}=${encodeURIComponent(search)}`;
+    }
+
+    axios.get(requestURL)
       .then((response) => {
         setPolicies(response.data);
       })
       .catch((error) => {
         console.error("데이터 가져오기 실패:", error);
       });
-  }, []);
+  }, [search, option]);
 
   useEffect(() => {
     const updatedPolicies =
       filter === "전체" ? policies : policies.filter((policy) => policy.category === filter);
+    
     setFilteredPolicies(updatedPolicies);
   }, [filter, policies]);
+
+  const storedCategory = M.data.storage("category");
+  const filterOptions = ["전체"];
+  if (storedCategory) {
+    filterOptions.push(...storedCategory);
+  }
 
   const handlePreviousPage = () => {
     if (currentPage > 1) {
@@ -66,7 +81,11 @@ const Custom = () => {
       const textWidth = context.measureText(text).width;
       element.style.width = textWidth + "px";
     });
-  }, [currentPage]);
+  }, [filteredPolicies, name, currentPage]);
+
+  const maxPageDisplay = 5;
+  const startPage = Math.max(1, currentPage - Math.floor(maxPageDisplay / 2));
+  const endPage = Math.min(pageNumbers, startPage + maxPageDisplay - 1);
 
   return (
     <div className="container">
@@ -114,13 +133,13 @@ const Custom = () => {
         >
           &lsaquo;
         </button>
-        {[...Array(pageNumbers)].map((_, number) => (
+        {Array.from({ length: endPage - startPage + 1 }, (_, i) => (
           <button
-            key={number + 1}
-            onClick={() => setCurrentPage(number + 1)}
-            className={`page-button ${currentPage === number + 1 ? "active" : ""}`}
+            key={startPage + i}
+            onClick={() => setCurrentPage(startPage + i)}
+            className={`page-button ${currentPage === startPage + i ? "active" : ""}`}
           >
-            {number + 1}
+            {startPage + i}
           </button>
         ))}
         <button
