a527a9ab25f758704f0ad82432a83541a359fb3e
suhye0n <contact.suhyeon@gmail.com>
2023-11-08 17:25:01 +0900

    feat: 정렬 기능 추가

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const Latest = () => {
     const [subsidiesData, setSubsidiesData] = useState([]);
+    const [sortOption, setSortOption] = useState("최신순");
 
     useEffect(() => {
         axios.get('/v1/subsidies-review/all')
@@ -16,16 +17,49 @@ const Latest = () => {
             });
     }, []);
 
+    const handleSortChange = (event) => {
+        setSortOption(event.target.value);
+    };
+
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
         return new Date(dateString).toLocaleString('ko-KR', options);
     };
 
+    const sortPolicies = (policies, sortOption) => {
+        switch (sortOption) {
+            case "제목순":
+                return policies.sort((a, b) => a.title.localeCompare(b.title));
+            case "조회수순":
+                return policies.sort((a, b) => b.views - a.views);
+            case "좋아요순":
+                return policies.sort((a, b) => b.likes - a.likes);
+            case "댓글순":
+                return policies.sort((a, b) => b.comments - a.comments);
+            default:
+                return policies.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+        }
+    };
+
+    const sortedPolicies = sortPolicies(subsidiesData, sortOption);
+
     return (
         <div className="container">
             <h3>후기글 모음</h3>
+            <select
+                id="filterSelect"
+                value={sortOption}
+                onChange={handleSortChange}
+                style={{ marginTop: '20px' }}
+            >
+                <option value="최신순">최신순</option>
+                <option value="제목순">제목순</option>
+                <option value="조회수순">조회수순</option>
+                <option value="좋아요순">좋아요순</option>
+                <option value="댓글순">댓글순</option>
+            </select>
             <ul className="policy-list">
-                {subsidiesData.map((policy) => (
+                {sortedPolicies.map((policy) => (
                     <li key={policy.id} className="policy-item">
                         <Link to={`/detail?id=${policy.id}&latest`}>
                             <div className="policy-details">
