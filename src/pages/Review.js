c29d69ea6b16b58de8ac46dae6c466fa0bef1835
suhye0n <contact.suhyeon@gmail.com>
2023-11-08 17:16:46 +0900

    feat: 후기글 axios 통신

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,61 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { BsHeart, BsHeartFill, BsChatLeftDots } from 'react-icons/bs';
-
-const policiesData = [
-    {
-        id: 1,
-        title: "청년도약계좌 신청 후기입니다!!",
-        subsidy: "청년도약계좌",
-        date: "2023-10-26 12:00",
-        liked: 3,
-        comment: 5
-    },
-    {
-        id: 2,
-        title: "청년도약계좌 신청 후기입니다!!",
-        subsidy: "청년도약계좌",
-        date: "2023-10-26 12:00",
-        liked: 3,
-        comment: 5
-    },
-    {
-        id: 3,
-        title: "청년도약계좌 신청 후기입니다!!",
-        subsidy: "청년도약계좌",
-        date: "2023-10-26 12:00",
-        liked: 3,
-        comment: 5
-    },
-    {
-        id: 4,
-        title: "청년도약계좌 신청 후기입니다!!",
-        subsidy: "청년도약계좌",
-        date: "2023-10-26 12:00",
-        liked: 3,
-        comment: 5
-    },
-];
+import { FaRegHeart, FaRegEye, FaRegCommentDots } from 'react-icons/fa';
+import axios from 'axios';
 
 const Latest = () => {
+    const [subsidiesData, setSubsidiesData] = useState([]);
+
+    useEffect(() => {
+        axios.get('/v1/subsidies-review/all')
+            .then((response) => {
+                setSubsidiesData(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+        return new Date(dateString).toLocaleString('ko-KR', options);
+    };
+
     return (
         <div className="container">
             <h3>후기글 모음</h3>
             <ul className="policy-list">
-                {policiesData.map((policy) => (
+                {subsidiesData.map((policy) => (
                     <li key={policy.id} className="policy-item">
                         <Link to={`/detail?id=${policy.id}&latest`}>
                             <div className="policy-details">
                                 <div className="policy-info">
+                                    <div className="review-count" style={{ float: 'none' }}>{policy.subsidy.title}</div>
                                     <div className="latest-title">
                                         {policy.title}
                                     </div>
-                                    <div className="latest-date">{policy.date}</div>
-                                </div>
-                                <div className='count'>
-                                    <span><BsHeart /> {policy.liked}</span>
-                                    <span><BsChatLeftDots /> {policy.comment}</span>
-                                    <div className="review-count">{policy.subsidy}</div>
+                                    <div className="latest-date">
+                                        {formatDate(policy.created_at)}&nbsp;
+                                        <span style={{ color: "#999" }}>({formatDate(policy.updated_at)} 수정)</span>
+                                    </div>
+                                    <div className='count views'>
+                                        <span><FaRegEye /> {policy.views}</span>
+                                        <span><FaRegHeart /> {policy.likes}</span>
+                                        <span><FaRegCommentDots /> {policy.views}</span>
+                                    </div>
                                 </div>
                             </div>
                         </Link>
