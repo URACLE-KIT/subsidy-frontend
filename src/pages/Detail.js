e4e2cb7188cbee5fc2a3556e923a4b8c3525843a
jh01475 <jh0147@hotmail.com>
2023-11-09 17:12:33 +0900

    fix: 후기 상세페이지 수정

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,31 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { FaBookmark, FaRegBookmark, FaExternalLinkAlt, FaPencilAlt, FaRegEye, FaRegCommentDots, FaRegTrashAlt, FaHeart, FaRegHeart } from 'react-icons/fa';
-import { BsBriefcase, BsTelephone, BsBoxArrowUpRight, BsLink45Deg, BsPersonFill, BsFileText, BsGift, BsCalendar } from 'react-icons/bs';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  FaBookmark,
+  FaRegBookmark,
+  FaExternalLinkAlt,
+  FaPencilAlt,
+  FaRegEye,
+  FaRegCommentDots,
+  FaRegTrashAlt,
+  FaHeart,
+  FaRegHeart,
+} from "react-icons/fa";
+import {
+  BsBriefcase,
+  BsTelephone,
+  BsBoxArrowUpRight,
+  BsLink45Deg,
+  BsPersonFill,
+  BsFileText,
+  BsGift,
+  BsCalendar,
+} from "react-icons/bs";
+import { Link, useLocation } from "react-router-dom";
 
-import Modal from '../layout/Modal';
+import Modal from "../layout/Modal";
 
 const Detail = () => {
   const [policy, setPolicy] = useState(null);
   const [review, setReview] = useState(null);
-  const [activeTab, setActiveTab] = useState('신청정보');
+  const [activeTab, setActiveTab] = useState("신청정보");
   const [modalOpen, setModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState(null);
-  const [userId, setUserId] = useState('');
+  const [userId, setUserId] = useState("");
   const [userScrappedPolicies, setUserScrappedPolicies] = useState([]);
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState(0);
 
   useEffect(() => {
-    const storedUserId = M.data.storage('id');
+    const storedUserId = M.data.storage("id");
     if (storedUserId) {
       setUserId(storedUserId);
     }
   }, []);
 
   useEffect(() => {
-    axios.get(`/v1/subsidyscraps/search/subsidyinfo?userId=${userId}`)
+    axios
+      .get(`/v1/subsidyscraps/search/subsidyinfo?userId=${userId}`)
       .then((response) => {
         setUserScrappedPolicies(response.data);
       })
@@ -36,7 +56,7 @@ const Detail = () => {
 
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const id = searchParams.get('id');
+  const id = searchParams.get("id");
 
   const openModal = (content) => {
     setModalContent(content);
@@ -55,10 +75,16 @@ const Detail = () => {
     const isBookmarked = isScrapped(policy.title);
 
     if (isBookmarked) {
-      const updatedUserScrappedPolicies = userScrappedPolicies.filter((policy) => policy.title !== policy.title);
+      const updatedUserScrappedPolicies = userScrappedPolicies.filter(
+        (policy) => policy.title !== policy.title
+      );
       setUserScrappedPolicies(updatedUserScrappedPolicies);
     } else {
-      const newPolicy = { id: policy.id, title: policy.title, description: policy.description };
+      const newPolicy = {
+        id: policy.id,
+        title: policy.title,
+        description: policy.description,
+      };
       const updatedUserScrappedPolicies = [...userScrappedPolicies, newPolicy];
       setUserScrappedPolicies(updatedUserScrappedPolicies);
     }
@@ -86,7 +112,8 @@ const Detail = () => {
 
   const toggleLike = () => {
     if (!isLiked) {
-      axios.put(`/v1/subsidies-review/increment-likes?id=${id}`)
+      axios
+        .put(`/v1/subsidies-review/increment-likes?id=${id}`)
         .then((response) => {
           setIsLiked(true);
           setLikes(likes + 1);
@@ -96,33 +123,41 @@ const Detail = () => {
           console.error("좋아요 증가 실패:", error);
         });
     }
-  };  
+  };
 
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
   };
 
   const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
-    return new Date(dateString).toLocaleString('ko-KR', options);
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    return new Date(dateString).toLocaleString("ko-KR", options);
   };
 
   useEffect(() => {
-    const isReview = searchParams.get('review');
+    
+    const isReview = searchParams.get("review");
 
     if (isReview !== null) {
       axios
-        .get(`/v1/subsidies-review/all`)
+        .get(`/v1/subsidies-review/search/reviewId?reviewId=${id}`)
         .then((response) => {
-          console.log(response.data)
+          console.log(response.data);
           setReview(response.data);
         })
         .catch((error) => {
           console.error(error);
         });
     } else {
+      setReview(null);
       axios
-        .get(`/v1/subsidies/id?id=${id}`)
+        .get(`/v1/subsidies/subsidyId?id=${id}`)
         .then((response) => {
           setPolicy(response.data);
         })
@@ -131,12 +166,13 @@ const Detail = () => {
         });
 
       if (!isPageViewed()) {
-        axios.put(`/v1/subsidies/increment-views?id=${id}`)
+        axios
+          .put(`/v1/subsidies/increment-views?id=${id}`)
           .then((response) => {
             console.log("조회 수 증가 성공:", response);
             viewedPages.push(id);
             M.data.storage({
-              'viewedPages': viewedPages
+              viewedPages: viewedPages,
             });
           })
           .catch((error) => {
@@ -146,7 +182,7 @@ const Detail = () => {
     }
   }, [location.search]);
 
-  const viewedPages = M.data.storage('viewedPages') || [];
+  const viewedPages = M.data.storage("viewedPages") || [];
 
   const isPageViewed = () => {
     return viewedPages.includes(id);
@@ -158,17 +194,24 @@ const Detail = () => {
         {policy && (
           <>
             <div className="header">
-              <div className='views'>
+              <div className="views">
                 <FaRegEye /> {policy.views}&nbsp;&nbsp;&nbsp;
-                <Link to={`/review?id=${id}`} style={{ color: '#999' }}>
+                <Link to={`/review?id=${id}`} style={{ color: "#999" }}>
                   <FaRegCommentDots /> {policy.views}
                 </Link>
               </div>
-              <button className="bookmark-button" onClick={toggleBookmark} style={{ boxShadow: 'none', width: 'auto', marginTop: '-50px' }}>
+              <button
+                className="bookmark-button"
+                onClick={toggleBookmark}
+                style={{ boxShadow: "none", width: "auto", marginTop: "-50px" }}
+              >
                 {isScrapped(policy.title) ? <FaBookmark /> : <FaRegBookmark />}
               </button>
               <div className="detail-title">{policy.title}</div>
-              <button className="detail-button" onClick={() => openModal('공유하기')}>
+              <button
+                className="detail-button"
+                onClick={() => openModal("공유하기")}
+              >
                 <FaExternalLinkAlt /> 공유하기
               </button>
               <Link to="/write?type=review">
@@ -183,23 +226,28 @@ const Detail = () => {
         {review && (
           <>
             <div className="header">
-              <div className="policy-date" style={{ maxWidth: "100%", display: "inline-block" }}>
-                {review[0].subsidy.title}
+              <div
+                className="policy-date"
+                style={{ maxWidth: "100%", display: "inline-block" }}
+              >
+                {review.subsidy.title}
               </div>
 
-              <div className='views' style={{ float: 'right' }}>
+              <div className="views" style={{ float: "right" }}>
                 <span>
                   <FaRegEye /> {review.views}
                 </span>
                 <span>
-                  <Link to={`/review?id=${id}`} style={{ color: '#999' }}>
-                    <FaRegCommentDots /> {review[0].views}
+                  <Link to={`/review?id=${id}`} style={{ color: "#999" }}>
+                    <FaRegCommentDots /> {review.views}
                   </Link>
                 </span>
-                {M.data.storage("name") === review[0].user.name && (
+                {M.data.storage("name") === review.user.name && (
                   <>
                     <span>
-                      <Link to="/write"><FaPencilAlt /></Link>
+                      <Link to={`/write?id=${id}&type=reviewedit`}>
+                        <FaPencilAlt />
+                      </Link>
                     </span>
                     <span>
                       <FaRegTrashAlt />
@@ -208,22 +256,34 @@ const Detail = () => {
                 )}
               </div>
 
-              <div className="detail-title">{review[0].title}</div>
-              <div className="policy-description">작성자: {review[0].user.name}</div>
-              <div className="latest-date">{formatDate(review[0].created_at)} <span style={{ color: "#999" }}>({formatDate(review[0].updated_at)} 수정)</span></div>
+              <div className="detail-title">{review.title}</div>
+              <div className="policy-description">
+                작성자: {review.user.name}
+              </div>
+              <div className="latest-date">
+                {formatDate(review.created_at)}{" "}
+                <span style={{ color: "#999" }}>
+                  ({formatDate(review.updated_at)} 수정)
+                </span>
+              </div>
 
-              <button className="detail-button" onClick={() => openModal('공유하기')}>
-                <FaExternalLinkAlt /> 공유하기
-              </button>
-              <Link to={`/detail?id=${review[0].subsidy.id}`}>
-                <button className="detail-button">
-                  <FaPencilAlt /> 원본글 가기
+              <div className="detail-button-group">
+                <button className="like-button" onClick={toggleLike}>
+                  {isLiked ? <FaHeart size={18} /> : <FaRegHeart size={18}/>}&nbsp;
+                  {review.likes}
                 </button>
-              </Link>
-              <button className="like-button" onClick={toggleLike}>
-                {isLiked ? <FaHeart /> : <FaRegHeart />}
-                {review[0].likes}
-              </button>
+                <button
+                  className="detail-button"
+                  onClick={() => openModal("공유하기")}
+                >
+                  <FaExternalLinkAlt /> 공유하기
+                </button>
+                <Link to={`/detail?id=${review.subsidy.id}`}>
+                  <button className="detail-button">
+                    <FaPencilAlt /> 원본글 가기
+                  </button>
+                </Link>
+              </div>
             </div>
           </>
         )}
@@ -237,78 +297,127 @@ const Detail = () => {
         <>
           <div className="tabs">
             <button
-              style={{ boxShadow: 'none', width: 'auto', marginTop: 0, borderRadius: 0 }}
-              className={`tab ${activeTab === '신청정보' ? 'active' : ''}`}
-              onClick={() => handleTabClick('신청정보')}
+              style={{
+                boxShadow: "none",
+                width: "auto",
+                marginTop: 0,
+                borderRadius: 0,
+              }}
+              className={`tab ${activeTab === "신청정보" ? "active" : ""}`}
+              onClick={() => handleTabClick("신청정보")}
             >
               신청정보
             </button>
             <button
-              style={{ boxShadow: 'none', width: 'auto', marginTop: 0, borderRadius: 0 }}
-              className={`tab ${activeTab === '신청문의' ? 'active' : ''}`}
-              onClick={() => handleTabClick('신청문의')}
+              style={{
+                boxShadow: "none",
+                width: "auto",
+                marginTop: 0,
+                borderRadius: 0,
+              }}
+              className={`tab ${activeTab === "신청문의" ? "active" : ""}`}
+              onClick={() => handleTabClick("신청문의")}
             >
               신청문의
             </button>
             <button
-              style={{ boxShadow: 'none', width: 'auto', marginTop: 0, borderRadius: 0 }}
-              className={`tab ${activeTab === '관련정보' ? 'active' : ''}`}
-              onClick={() => handleTabClick('관련정보')}
+              style={{
+                boxShadow: "none",
+                width: "auto",
+                marginTop: 0,
+                borderRadius: 0,
+              }}
+              className={`tab ${activeTab === "관련정보" ? "active" : ""}`}
+              onClick={() => handleTabClick("관련정보")}
             >
               관련정보
             </button>
             <button
-              style={{ boxShadow: 'none', width: 'auto', marginTop: 0, borderRadius: 0 }}
-              className={`tab ${activeTab === '신청방법' ? 'active' : ''}`}
-              onClick={() => handleTabClick('신청방법')}
+              style={{
+                boxShadow: "none",
+                width: "auto",
+                marginTop: 0,
+                borderRadius: 0,
+              }}
+              className={`tab ${activeTab === "신청방법" ? "active" : ""}`}
+              onClick={() => handleTabClick("신청방법")}
             >
               신청방법
             </button>
           </div>
 
           <div className="tab-content">
-            {activeTab === '신청정보' && (
+            {activeTab === "신청정보" && (
               <div className="tab-panel">
                 <h3>신청정보</h3>
-                <div className='tab-item'>
-                  <span className="sub-title"><BsCalendar />&nbsp;&nbsp; 신청기간</span>{policy.application_period}
+                <div className="tab-item">
+                  <span className="sub-title">
+                    <BsCalendar />
+                    &nbsp;&nbsp; 신청기간
+                  </span>
+                  {policy.application_period}
                 </div>
-                <div className='tab-item'>
-                  <span className="sub-title"><BsGift />&nbsp;&nbsp; 지원형태</span>{policy.support_type}
+                <div className="tab-item">
+                  <span className="sub-title">
+                    <BsGift />
+                    &nbsp;&nbsp; 지원형태
+                  </span>
+                  {policy.support_type}
                 </div>
               </div>
             )}
-            {activeTab === '신청문의' && (
+            {activeTab === "신청문의" && (
               <div className="tab-panel">
                 <h3>신청문의</h3>
 
-                <div className='tab-item'>
-                  <span className="sub-title"><BsTelephone />&nbsp;&nbsp; 전화문의</span>{policy.telephone_inquiry}
+                <div className="tab-item">
+                  <span className="sub-title">
+                    <BsTelephone />
+                    &nbsp;&nbsp; 전화문의
+                  </span>
+                  {policy.telephone_inquiry}
                 </div>
               </div>
             )}
-            {activeTab === '관련정보' && (
+            {activeTab === "관련정보" && (
               <div className="tab-panel">
                 <h3>관련정보</h3>
 
-                <div className='tab-item'>
-                  <span className="sub-title"><BsFileText />&nbsp;&nbsp; 카테고리</span>{policy.category}
+                <div className="tab-item">
+                  <span className="sub-title">
+                    <BsFileText />
+                    &nbsp;&nbsp; 카테고리
+                  </span>
+                  {policy.category}
                 </div>
-                <div className='tab-item'>
-                  <span className="sub-title"><BsLink45Deg />&nbsp;&nbsp; 상세정보</span>
-                  <Link to={policy.detail_information_url}>바로가기 <BsBoxArrowUpRight /></Link>
+                <div className="tab-item">
+                  <span className="sub-title">
+                    <BsLink45Deg />
+                    &nbsp;&nbsp; 상세정보
+                  </span>
+                  <Link to={policy.detail_information_url}>
+                    바로가기 <BsBoxArrowUpRight />
+                  </Link>
                 </div>
               </div>
             )}
-            {activeTab === '신청방법' && (
+            {activeTab === "신청방법" && (
               <div className="tab-panel">
                 <h3>신청방법</h3>
-                <div className='tab-item'>
-                  <span className="sub-title"><BsPersonFill />&nbsp;&nbsp; 신청방법</span>
-                  <Link to={policy.application_process_url}>{policy.application_process} <BsBoxArrowUpRight /></Link>
+                <div className="tab-item">
+                  <span className="sub-title">
+                    <BsPersonFill />
+                    &nbsp;&nbsp; 신청방법
+                  </span>
+                  <Link to={policy.application_process_url}>
+                    {policy.application_process} <BsBoxArrowUpRight />
+                  </Link>
                 </div>
-                <div className='tab-item'>
-                  <span className="sub-title"><BsBriefcase />&nbsp;&nbsp; 접수기관</span>
+                <div className="tab-item">
+                  <span className="sub-title">
+                    <BsBriefcase />
+                    &nbsp;&nbsp; 접수기관
+                  </span>
                   {policy.receiving_agency}
                 </div>
               </div>
@@ -317,11 +426,7 @@ const Detail = () => {
         </>
       )}
 
-      {review && (
-        <div className="tab-content">
-          {review[0].content}
-        </div>
-      )}
+      {review && <div className="tab-content">{review.content}</div>}
     </>
   );
 };
