4f7521ffaab63f517b7ae9370667fe774e31e5de
suhye0n <contact.suhyeon@gmail.com>
2023-11-16 15:14:44 +0900

    fix: 연령대 수정

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -119,6 +119,7 @@ const Profile = () => {
         M.data.storage({ 'name': updatedName });
         M.data.storage({ 'birthday': updatedBirthday });
         M.data.storage({ 'maritalStatus': uwedding });
+        M.data.storage({ 'lifecycle': response.data.lifecycle });
 
         navigate('/mypage');
       }
