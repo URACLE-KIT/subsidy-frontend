6939e3857a3b12c056e20abbf0d03b9b105bcb8e
suhye0n <contact.suhyeon@gmail.com>
2023-11-17 12:55:51 +0900

    fix: 백분율로 표시되도록 수정

--- src/layout/CategoryChart.js
+++ src/layout/CategoryChart.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { ResponsivePie } from '@nivo/pie';
 
+const transformData = (data) => {
+  const clonedData = [...data];
+
+  clonedData.sort((a, b) => b.value - a.value);
+
+  const otherSum = clonedData.slice(3).reduce((acc, entry) => acc + entry.value, 0);
+
+  const transformedData = clonedData.slice(0, 3).concat({
+    id: '기타',
+    value: otherSum,
+  });
+
+  const total = transformedData.reduce((acc, entry) => acc + entry.value, 0);
+
+  const finalData = transformedData.map(entry => ({
+    ...entry,
+    value: parseFloat((entry.value / total * 100).toFixed(2)),
+  }));
+
+  return finalData;
+};
+
 const CategoryChart = ({ data }) => {
+  const transformedData = transformData(data);
+
   return (
     <div style={{ height: '400px' }}>
       <ResponsivePie
-        data={data}
+        data={transformedData}
         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
         innerRadius={0.5}
         padAngle={0.7}
@@ -23,6 +47,7 @@ const CategoryChart = ({ data }) => {
         radialLabelsLinkColor={{ from: 'color' }}
         sliceLabelsSkipAngle={10}
         sliceLabelsTextColor="#333333"
+        sliceLabel={(entry) => `${entry.value.toFixed(2)}%`}
       />
     </div>
   );
