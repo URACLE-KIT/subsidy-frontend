5511939a46059b7f0a3a015f534e93e9115f7c43
suhye0n <contact.suhyeon@gmail.com>
2023-11-20 15:32:59 +0900

    style: 스타일 변경

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -1,17 +1,19 @@
 import React from "react";
+import { FaDownload } from 'react-icons/fa';
 
 const Download = () => {
     return (
         <div className="container" style={{ textAlign: 'center' }}>
-            앱을 다운로드 해주세요!
+            <p>앱을 다운로드 해주세요!</p>
             
             <br />
 
             <a
                 href={process.env.PUBLIC_URL + '/보조알리미.apk'}
                 download="보조알리미.apk"
-                className="category-button"
+                className="download-button"
             >
+                <FaDownload className="download-icon" />
                 다운로드
             </a>
         </div>
