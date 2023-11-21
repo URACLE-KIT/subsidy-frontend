3165996fb6561803c49a33b5756ac06157252f11
suhye0n <contact.suhyeon@gmail.com>
2023-11-21 10:08:03 +0900

    fix: 뒤로가기 여러번 호출되는 문제 해결

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+window.addEventListener('load', () => {
+    const onBackEvent = new Event('onBack')
+    M.onBack(() => {
+        window.dispatchEvent(onBackEvent)
+    })
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
